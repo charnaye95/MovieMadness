2efc8722a8dbaa927295e8ac782214cd16061da9
Charnaye Grier <charnayegrier@gmail.com>
2022-03-09 00:52:56 -0500

    First commit - set up homepage and whiteboarded a bit

---
+++ app.js
@@ -0,0 +1,6 @@
+//selecting elements to manipulate
+let scoreboard = document.querySelector('#score');
+let filmButton = document.querySelector('#film');
+let musicButton = document.querySelector ('#music');
+let tvButton = document.querySelector('#television');
+let videoGamesButton = document.querySelector('#videoGames'); 
---
+++ whiteboarding.js
@@ -0,0 +1,6 @@
+// Determine page formats - popup or new page or link it dynamically with html for questions to appear
+// connect API: https://opentdb.com/
+//pull questions from API at random
+//pull corresponding multiple choice answers 
+// Make buttons clickable
+//function to add money to the bank when a question is correct
