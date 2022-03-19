5f284eead42476a7d9a118980b2cc86be8df1619
Charnaye Grier <charnayegrier@gmail.com>
2022-03-18 22:59:13 -0400

    adding to readme

--- README.md
+++ README.md
@@ -4,25 +4,29 @@
 
 
 ## Screenshots
-![alt text](link to image)
-![alt text](link to image)
+![Home](./startscreen.png)
+![Game](./game.png)
+
 
 ## Technologies Used:
 -HTML
 -CSS
 -JavaScript
 
-Trivia
-
 ## User Stories
 
 ## Wireframes
+![Home Frame](./wireframe1.png)
+![Game Frame](./wireframe2.png)
 
 ## Unsolved Problems or Major Hurdles
+-creating a timer to start over per question
+-using API to pull in answers, I had the questions but trying to pull multiple choice answers was difficult
+-getting the correct answer to highlight or alert the correct answer when the question is answer incorrectly
 
 
-# Sources Used: 
-https://stackoverflow.com/questions/59037553/fetch-multiple-urls-at-the-same-time
+## Sources Used: 
+
 https://unsplash.com/photos/WevidclYpdc
 https://code-boxx.com/allow-one-click-javascript/
 https://bobbyhadz.com/blog/javascript-change-background-color-on-click
--- app.js
+++ app.js
@@ -24,64 +24,64 @@ filmQuestionsArr = [
 
 filmAnswers = [
     [
-        {text: "A: The Revenant", correct: true},
-        {text: "B: What's Eating Gilbert Grape", correct: false},
-        {text: "C: Titanic", correct: false},
-        {text: "D: The Wolf of Wall Street", correct: false}
+        { text: "A: The Revenant", correct: true },
+        { text: "B: What's Eating Gilbert Grape", correct: false },
+        { text: "C: Titanic", correct: false },
+        { text: "D: The Wolf of Wall Street", correct: false }
     ],
     [
-        {text: "A: Avengers: Endgame", correct: false},
-        {text: "B: Frozen", correct: false},
-        {text: "C: Avatar", correct: true},
-        {text: "D: Star Wars: The Force Awakens", correct: false}
+        { text: "A: Avengers: Endgame", correct: false },
+        { text: "B: Frozen", correct: false },
+        { text: "C: Avatar", correct: true },
+        { text: "D: Star Wars: The Force Awakens", correct: false }
     ],
     [
-        {text: "A: Meryl Streep", correct: false},
-        {text: "B: Rita Moreno", correct: true},
-        {text: "C: Denzel Washington", correct: false},
-        {text: "D: Robert De Niro", correct: false}
+        { text: "A: Meryl Streep", correct: false },
+        { text: "B: Rita Moreno", correct: true },
+        { text: "C: Denzel Washington", correct: false },
+        { text: "D: Robert De Niro", correct: false }
     ],
     [
-        {text: "A: Willem Dafoe", correct: true},
-        {text: "B: Heath Ledger", correct: false},
-        {text: "C: Joaquin Phoenix", correct: false},
-        {text: "D: Jared Leto", correct: false}
+        { text: "A: Willem Dafoe", correct: true },
+        { text: "B: Heath Ledger", correct: false },
+        { text: "C: Joaquin Phoenix", correct: false },
+        { text: "D: Jared Leto", correct: false }
     ],
     [
-        {text: "A: Scarlett Johansson", correct: false},
-        {text: "B: Idina Menzel", correct: false},
-        {text: "C: Reese Witherspoon", correct: false},
-        {text: "D: Kristen Bell", correct: true}
+        { text: "A: Scarlett Johansson", correct: false },
+        { text: "B: Idina Menzel", correct: false },
+        { text: "C: Reese Witherspoon", correct: false },
+        { text: "D: Kristen Bell", correct: true }
     ],
     [
-        {text: "A: Jane Fonda", correct: false},
-        {text: "B: Audrey Hepburn", correct: true},
-        {text: "C: Marilyn Monroe", correct: false},
-        {text: "D: Julie Andrews", correct: false}
+        { text: "A: Jane Fonda", correct: false },
+        { text: "B: Audrey Hepburn", correct: true },
+        { text: "C: Marilyn Monroe", correct: false },
+        { text: "D: Julie Andrews", correct: false }
     ],
     [
-        {text: "A: Christopher Nolan", correct: false},
-        {text: "B: David Fincher", correct: true},
-        {text: "C: Quentin Tarantino", correct: false},
-        {text: "D: Martin Scorsese", correct: false}
+        { text: "A: Christopher Nolan", correct: false },
+        { text: "B: David Fincher", correct: true },
+        { text: "C: Quentin Tarantino", correct: false },
+        { text: "D: Martin Scorsese", correct: false }
     ],
     [
-        {text: "A: Drama", correct: false},
-        {text: "B: Comedy", correct: false},
-        {text: "C: Action", correct: false},
-        {text: "D: Adventure", correct: true} 
+        { text: "A: Drama", correct: false },
+        { text: "B: Comedy", correct: false },
+        { text: "C: Action", correct: false },
+        { text: "D: Adventure", correct: true }
     ],
     [
-        {text: "A: Muhammad Ali", correct: false},
-        {text: "B: Richard Williams", correct: false},
-        {text: "C: Chris Gardner", correct: false},
-        {text: "D: Malcolm X", correct: true}
+        { text: "A: Muhammad Ali", correct: false },
+        { text: "B: Richard Williams", correct: false },
+        { text: "C: Chris Gardner", correct: false },
+        { text: "D: Malcolm X", correct: true }
     ],
     [
-        {text: "A: Sony", correct: false},
-        {text: "B: Paramount", correct: false},
-        {text: "C: Disney", correct: true},
-        {text: "D: Netflix", correct: false}
+        { text: "A: Sony", correct: false },
+        { text: "B: Paramount", correct: false },
+        { text: "C: Disney", correct: true },
+        { text: "D: Netflix", correct: false }
     ],
 
 ]
@@ -119,39 +119,39 @@ playGame();
 
 //function to check the answer is right or wrong to a question
 function checkAnswer(event) {
-       let index = parseInt(event.target.id)       
+    let index = parseInt(event.target.id)
     if (filmAnswers[random][index].correct === true) {
-             event.target.style.backgroundColor = "green"
-                scoreboard.innerHTML = `BANK $${score += 100}`;
-                alert("That is correct! Click next!")
-                for (let j = 0; j < choices.length; j++) {
-                    choices[j].disabled = true;
+        event.target.style.backgroundColor = "green"
+        scoreboard.innerHTML = `BANK $${score += 100}`;
+        alert("That is correct! Click next!")
+        for (let j = 0; j < choices.length; j++) {
+            choices[j].disabled = true;
         }
-    }  else {
-                alert("Incorrect. Better luck next time. Click next!")
-                for (let k = 0; k < choices.length; k++) {
-                    choices[k].disabled = true;
-            }
+    } else {
+        alert("Incorrect. Better luck next time. Click next!")
+        for (let k = 0; k < choices.length; k++) {
+            choices[k].disabled = true;
         }
-        win();
     }
+    win();
+}
 
 
 //trying to tie a function to clicking the next button that will then show the next randomized question in the array
 function nextQuestion() {
-        playGame(); 
-for (let l = 0; l < choices.length; l++) {
-            choices[l].disabled = false;
-            choices[l].style.backgroundColor = "rgb(221, 69, 22";
-        }
+    playGame();
+    for (let l = 0; l < choices.length; l++) {
+        choices[l].disabled = false;
+        choices[l].style.backgroundColor = "rgb(221, 69, 22";
     }
+}
 
 // winning conditons
 function win() {
     if (noRepeatQs.length == 10 && score === 800 || noRepeatQs.length == 10 && score === 900) {
-    alert(`You finished with a score of ${score} and WON! You are a true film buff! Try for a higher score by clicking reset.`)
-} else if (noRepeatQs.length == 10 && score < 800) { 
-    alert(`You finished with a score of ${score} and lost. Try again by clicking reset.`)
+        alert(`You finished with a score of ${score} and WON! You are a true film buff! Try for a higher score by clicking reset.`)
+    } else if (noRepeatQs.length == 10 && score < 800) {
+        alert(`You finished with a score of ${score} and lost. Try again by clicking reset.`)
     } else if (noRepeatQs.length == 10 && score === 1000) {
         alert("You have finished with a score of 1000!!! You are a master cinema enthusiast")
     }
@@ -160,7 +160,7 @@ function win() {
 function resetGame() {
     noRepeatQs = [];
     scoreboard.innerHTML = `BANK $${score = 0}`;
-        playGame();
+    playGame();
     for (let m = 0; m < choices.length; m++) {
         choices[m].disabled = false;
         choices[m].style.backgroundColor = "rgb(221, 69, 22";
